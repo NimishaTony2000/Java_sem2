b1ae570b2b3c88c0ba6a3b917aee9aa3f2ab15e2
NimishaTony2000 <95517102+NimishaTony2000@users.noreply.github.com>
2022-03-31 15:24:11 +0530

    Add files via upload

---
+++ Blaaaa.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg21mca033;
+import pkg21mca033.Hello;
+/**
+ *
+ * @author sjcet
+ */
+public class Blaaaa {
+    public static void main(String[] args) {
+        Hello obj = new Hello();
+        obj.display();
+    }
+}
