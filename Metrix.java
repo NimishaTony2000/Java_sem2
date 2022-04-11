89ce0bad07c56c90c88221955f1095a2d86a950b
NimishaTony2000 <95517102+NimishaTony2000@users.noreply.github.com>
2022-04-11 06:40:48 +0530

    /CO1

--- Metrix.java
+++ Metrix.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package pkg21mca033.CO1;
+package nimisha.tony;
 
 import java.util.Scanner;
 
 /**
  *
- * @author sjcet
+ * @author Nimisha
  */
 public class Metrix {
     public static void main(String args[])
--- Product.java
+++ Product.java
@@ -1,15 +1,14 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package pkg21mca033.CO1;
+package nimisha.tony;
 
 import java.util.Scanner;
 
 /**
  *
- * @author sjcet
+ * @author Nimisha
  */
 public class Product {
     int p_code;
@@ -59,5 +58,5 @@ public class Product {
                 System.out.println("Lowest Price is:"+b);
             }
         }
-    }
+    }   
 }
